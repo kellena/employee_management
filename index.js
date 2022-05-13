dce688f0dee98c5eba469aa275b2b717d956d485
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-12 19:53:15 -0500

    adjusted index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require ('inquirer');
 const db = require('./db/connection')
 
 require('dotenv').config();
-const { allDepts, allRoles, allEmps, addEmp, addDept, addRole, updateRole, finished} = require('./db/server')
+const { allDepts, allRoles, allEmps, addDept, addRole, addEmp, updateRole, finished} = require('./db/server')
 db.connect(function(err){
     if (err) throw err
     runPrompt()

ecb8c9fe74df57caaef96424e8b20b1778349c76
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-12 19:10:38 -0500

    changed schema.sql for clarity

--- db/schema.sql
+++ db/schema.sql
@@ -1,34 +1,28 @@
-DROP DATABASE IF EXISTS employee_tracker_db;
-CREATE database employee_management_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE employee_management_db;
+USE employee_db;
 
-CREATE TABLE department (
-  id INT AUTO_INCREMENT NOT NULL,
-  dept VARCHAR(30),
-  PRIMARY KEY (id)
+CREATE TABLE emp_department (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30) NOT NULL
 );
 
-USE employee_management_db;
-
-CREATE TABLE roles (
-  id INT AUTO_INCREMENT NOT NULL,
-  title VARCHAR(30),
-  salary DECIMAL(10,2),
-  department_id INT,
-  PRIMARY KEY (id), 
-  FOREIGN KEY (department_id) REFERENCES department(id)
+CREATE TABLE emp_role (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INT NOT NULL,
+FOREIGN KEY (department_id)
+REFERENCES emp_department(id)
 );
 
-USE employee_management_db;
-
-CREATE TABLE employees (
-  id INT AUTO_INCREMENT NOT NULL,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  roles_id INT, 
-  manager_id INT,
-  PRIMARY KEY (id),
-FOREIGN KEY (roles_id) REFERENCES roles(id),
-FOREIGN KEY (manager_id) REFERENCES employees(id)
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT NOT NULL,
+FOREIGN KEY (manager_id)
+REFERENCES employee(id)
 );

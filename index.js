4ce5c8dff5239910c687db82a1d4afb7ec93b4c6
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-12 19:16:10 -0500

    added require dotenv code within index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const inquirer = require ('inquirer');
 const db = require('./db/connection')
+
+require('dotenv').config();
+const { allDepts, allRoles, allEmps, addEmp, addDept, addRole, updateRole, finished} = require('./db/server')
+db.connect(function(err){
+    if (err) throw err
+    runPrompt()
+})
+

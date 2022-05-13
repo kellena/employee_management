9aefd7de1553d8f272d9aec624da30f898e8303f
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-12 19:52:59 -0500

    adjusted connection.js, finished building index.js

--- index.js
+++ index.js
@@ -8,3 +8,44 @@ db.connect(function(err){
     runPrompt()
 })
 
+function runPrompt () {
+    inquirer.prompt([
+        {type:"list",
+         name: "options",
+         message: "What would you like to do?",
+         choices: options
+        },
+    ])
+        .then ((answer) => {
+            console.log(answer);
+            switch (answer.options) {
+                case options[0]:
+                    allDepts();
+                    break;
+                case options[1]:
+                    allRoles();
+                    break;
+                case options[2]:
+                    allEmps();
+                    break;   
+                case options[3]:
+                    addDept();
+                    break;
+                case options[4]:
+                    addRole();
+                    break;
+                case options[5]:
+                    addEmp();
+                    break;
+                case options[6]:
+                    updateRole();
+                    break;
+                case options[7]:
+                    finished();
+                    break;
+            }
+        }) 
+}
+
+
+module.exports = runPrompt

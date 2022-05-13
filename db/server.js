089637e8518003d05409de6197a5fc5051f7595e
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-12 20:49:56 -0500

    built out addRole function

--- db/server.js
+++ db/server.js
@@ -27,9 +27,55 @@ function allEmps() {
     });
 }
 
-function addDept() {}
+function addDept() {
+    runPrompt = require('../index')
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'addDept',
+            message: "What is the name of the department you would like to add?"
 
-function addRole() {}
+        }
+    ]).then(response =>{
+        db.query('INSERT INTO emp_department SET ?', {
+            name: response.addDept
+        })
+        runPrompt();
+    })
+}
+
+function addRole() {
+    runPrompt = require('../index')
+    db.query('SELECT * FROM emp_department', (err, res)=>{
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'deptId',
+                message: 'What department is your new role in?',
+                choices: res.map(department=>department.name)
+            },
+            {
+                type: 'input',
+                name: 'addSalary',
+                message: "What is the salary of your new role?",
+            },
+            {
+                type: 'input',
+                name: "addTitle",
+                message: "What is the title of your new role?"
+            }
+        ]).then(response=>{
+            const selectedDept = res.find(department=>department.name === response.deptId)
+            db.query('INSERT INTO emp_role SET ?', {
+                salary: response.addSalary,
+                title: response.addTitle,
+                department_id: selectedDept.id
+            })
+            console.log('New role added!')
+            runPrompt();
+        })
+    })
+}
 
 function addEmp () {}
 

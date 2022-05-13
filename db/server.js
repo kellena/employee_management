174c45dd1f779321527b6e994bd261a7151f53d0
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-12 19:56:41 -0500

    put in basic skeleton to server.js for according functions from index.js, exporting all functions

---
+++ db/server.js
@@ -0,0 +1,31 @@
+const db = require('./connection')
+const inquirer = require('inquirer')
+
+let runPrompt;
+
+function allDepts() {
+    runPrompt = require('../index')
+    db.query('SELECT * FROM emp_department', function (err, results) {
+        console.table(results);
+        runPrompt();
+    });
+}
+
+function allRoles() {}
+
+function allEmps() {}
+
+function addDept() {}
+
+function addRole() {}
+
+function addEmp () {}
+
+function updateRole () {}
+
+function finished () {}
+
+
+module.exports = {
+    allDepts, allRoles, allEmps, addDept, addRole, addEmp, updateRole, finished
+}
